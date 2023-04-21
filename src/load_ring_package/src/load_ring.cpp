33e5e91217f2b2bf2247f89af3340f291b4f67ab
shohkoba <78463372+shohkoba@users.noreply.github.com>
2023-04-22 00:47:23 +0900

    アームにEnable機能を追加

--- src/load_ring_package/src/load_ring.cpp
+++ src/load_ring_package/src/load_ring.cpp
@@ -2,6 +2,7 @@
 #include<fabot_msgs/ArmMsg.h>
 #include<sensor_msgs/Joy.h>
 
+#define ENABLE_BUTTON 5
 #define CLOSE_HAND_BUTTON 0 //Xボタン
 #define OPEN_HAND_BUTTON 3 //Bボタン
 #define UP_ARM_BUTTON 1 //Yボタン
@@ -14,21 +15,27 @@ fabot_msgs::ArmMsg arm_state_msg;
 //ジョイコンのコールバック関数
 void joyCallback(const sensor_msgs::Joy::ConstPtr& joy)
 {
-    //両方押してるときは手は停止
-    if(joy->buttons[OPEN_HAND_BUTTON]==1 && joy->buttons[CLOSE_HAND_BUTTON]==0){
-        arm_state_msg.hand = 1;
-    }else if(joy->buttons[CLOSE_HAND_BUTTON]==1 && joy->buttons[OPEN_HAND_BUTTON]==0){
-        arm_state_msg.hand = 2;
-    }else{
-        arm_state_msg.hand = 0;
-    }
+    if (joy->buttons[ENABLE_BUTTON]) {
+        //両方押してるときは手は停止
+        if(joy->buttons[OPEN_HAND_BUTTON]==1 && joy->buttons[CLOSE_HAND_BUTTON]==0){
+            arm_state_msg.hand = 1;
+        }else if(joy->buttons[CLOSE_HAND_BUTTON]==1 && joy->buttons[OPEN_HAND_BUTTON]==0){
+            arm_state_msg.hand = 2;
+        }else{
+            arm_state_msg.hand = 0;
+        }
 
-    //両方押してるときは腕は停止
-    if(joy->buttons[UP_ARM_BUTTON]==1 && joy->buttons[DOWN_ARM_BUTTON]==0){
-        arm_state_msg.arm = 1;
-    }else if(joy->buttons[DOWN_ARM_BUTTON]==1 && joy->buttons[UP_ARM_BUTTON]==0){
-        arm_state_msg.arm = 2;
-    }else{
+        //両方押してるときは腕は停止
+        if(joy->buttons[UP_ARM_BUTTON]==1 && joy->buttons[DOWN_ARM_BUTTON]==0){
+            arm_state_msg.arm = 1;
+        }else if(joy->buttons[DOWN_ARM_BUTTON]==1 && joy->buttons[UP_ARM_BUTTON]==0){
+            arm_state_msg.arm = 2;
+        }else{
+            arm_state_msg.arm = 0;
+        }
+    }
+    else {
+        arm_state_msg.hand = 0;
         arm_state_msg.arm = 0;
     }
 }
